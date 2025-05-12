1ce7b77dcfe87bf1777557d47c568b171b60cdf1
Mr. Herbert <dherbert@fccsc.k12.in.us>
2025-05-12 13:29:45 -0400

    btn fix

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const Q2 = document.getElementById("Q2Grade");
 const E = document.getElementById("examGrade");
 const S = document.getElementById("semesterGrade");
 
-calculate.addEventListener("click", calculate);
+calculateBtn.addEventListener("click", calculate);
 
 function calculate() {
     const result = (Q1.value * 0.45) + (Q2.value * 0.45) + (E.value * 0.1);
-    S.innerText = result;
+    S.textContent = result;
 }
