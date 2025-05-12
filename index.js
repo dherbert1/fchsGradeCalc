98031939a8c51d21e7d83b877146c5f16142da8a
Mr. Herbert <dherbert@fccsc.k12.in.us>
2025-05-12 14:09:48 -0400

    warning

--- index.js
+++ index.js
@@ -3,10 +3,18 @@ const Q1 = document.getElementById("Q1Grade");
 const Q2 = document.getElementById("Q2Grade");
 const E = document.getElementById("examGrade");
 const S = document.getElementById("semesterGrade");
+const warning = document.getElementById("warning");
 
 calculateBtn.addEventListener("click", calculate);
 
 function calculate() {
-    const result = Math.round((Q1.value * 0.45) + (Q2.value * 0.45) + (E.value * 0.1));
-    S.textContent = `${result}`;
+    if(Q1.value > 100 || Q2.value > 100 || E.value > 100){
+        warning.setAttribute('display', 'block');
+    }
+    else{
+        warning.setAttribute('display', 'hidden');
+        const result = Math.round((Q1.value * 0.45) + (Q2.value * 0.45) + (E.value * 0.1));
+        S.textContent = `${result}`;
+    }
+    
 }
