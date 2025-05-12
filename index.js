3faafc6ac053eef8dee6c0679eb2b8d92563ea35
Mr. Herbert <dherbert@fccsc.k12.in.us>
2025-05-12 13:20:57 -0400

    eventListener

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const Q1 = document.getElementById("Q1Grade");
 const Q2 = document.getElementById("Q2Grade");
 const E = document.getElementById("examGrade");
 const S = document.getElementById("semesterGrade");
+
+calculate.addEventListener("click", calculate);
+
+function calculate() {
+    const result = (Q1.value * 0.45) + (Q2.value * 0.45) + (E.value * 0.1);
+    S.innerText = result;
+}
