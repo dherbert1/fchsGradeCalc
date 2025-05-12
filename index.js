26f82352c6c50fbe925202ee21169d1d372ad8dc
Mr. Herbert <dherbert@fccsc.k12.in.us>
2025-05-12 14:43:48 -0400

    display

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ calculateBtn.addEventListener("click", calculate);
 
 function calculate() {
     if(Q1.value > 100 || Q2.value > 100 || E.value > 100){
-        warning.style.visibility = 'visible';
+        warning.style.display = 'none';
     }
     else{
-        warning.style.visibility = 'hidden';
+        warning.style.display = 'block';
         const result = Math.round((Q1.value * 0.45) + (Q2.value * 0.45) + (E.value * 0.1));
         S.textContent = `${result}`;
     }
