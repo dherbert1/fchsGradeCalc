2b3759bf9c7d45d049707b512b32c6fc623d45b2
Mr. Herbert <dherbert@fccsc.k12.in.us>
2025-05-12 13:17:04 -0400

    JS starting

---
+++ index.js
@@ -0,0 +1,5 @@
+const calculateBtn = document.getElementById("calculate");
+const Q1 = document.getElementById("Q1Grade");
+const Q2 = document.getElementById("Q2Grade");
+const E = document.getElementById("examGrade");
+const S = document.getElementById("semesterGrade");
