350d53249541e54d94bc180c4672f890cb1fc58e
Mr. Herbert <dherbert@fccsc.k12.in.us>
2025-05-13 10:38:37 -0400

    subtraction

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function calculate() {
     }
 
     if(Math.round((Q1.value * 0.45) + (Q2.value * 0.45) <= 60)){
-        const scoreAtLeast = (60-(Q1.value * 0.45) + (Q2.value * 0.45))/.10;
+        const scoreAtLeast = (60-(Q1.value * 0.45) - (Q2.value * 0.45))/.10;
         danger.style.display = 'block';
         danger.style.height = 'auto';
         sAL.textContent = `${scoreAtLeast}`;
