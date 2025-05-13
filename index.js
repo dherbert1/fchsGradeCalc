6074433213f6dc01faa16d22046ad679fbbc48bd
Mr. Herbert <dherbert@fccsc.k12.in.us>
2025-05-13 10:34:31 -0400

    SAL

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const Q2 = document.getElementById("Q2Grade");
 const E = document.getElementById("examGrade");
 const S = document.getElementById("semesterGrade");
 const warning = document.getElementById("warning");
+const dZ = document.getElementById("dangerZone");
+const sAL = document.getElementById("scoreAtLeast");
+const danger = document.getElementById("danger");
 
 calculateBtn.addEventListener("click", calculate);
 
@@ -17,6 +20,13 @@ function calculate() {
         warning.style.height = '0';
         const result = Math.round((Q1.value * 0.45) + (Q2.value * 0.45) + (E.value * 0.1));
         S.textContent = `${result}`;
+    }
+
+    if(Math.round((Q1.value * 0.45) + (Q2.value * 0.45) <= 60)){
+        const scoreAtLeast = (60-(Q1.value * 0.45) + (Q2.value * 0.45))/.10;
+        danger.style.display = 'block';
+        danger.style.height = 'auto';
+        sAL.textContent = `$scoreAtLeast`;
     }
     
 }
